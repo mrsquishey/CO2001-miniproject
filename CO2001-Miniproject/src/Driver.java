ffb97ae44d316c9f4e3560f37595a77066467a32
James <James@DESKTOP-K7P7NB4>
2017-11-14 11:15:20 +0000

    Commit 2

--- CO2001-Miniproject/src/Driver.java
+++ CO2001-Miniproject/src/Driver.java
@@ -1,9 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Driver {
-
+    static private Movie movie1 = new Movie();
+    static private Movie movie2 = new Movie();
+    static private Movie movie3 = new Movie();
+    static private Movie movie4 = new Movie();
+    static private Movie movie5 = new Movie();
+    static private Movie movie6 = new Movie();
+    static private Movie movie7 = new Movie();
+    static private Movie movie8 = new Movie();
+    static private Movie movie9 = new Movie();
+    static private Movie movie10 = new Movie();
+    
 	public static void main(String[] args) {
 		
-
+		movie1.setIdAndTitle();
+		movie2.setIdAndTitle();
+		System.out.println(movie1.toString()+", "+movie2.toString());
+		
 	}
+	
+	
 
 }
--- CO2001-Miniproject/src/Movie.java
+++ CO2001-Miniproject/src/Movie.java
@@ -1,30 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Random;
 public class Movie {
 	
 	private int movieID, imdbID, tmdbID;
 	private String title, genres, timeStamp, tag;
 	private double rating;	
-	private HashMap <Integer, Movie> movieList = new HashMap<Integer, Movie>();
+	static private HashMap <Object, ArrayList<Object>> movieList = new HashMap <Object, ArrayList<Object>>();
+	static private String links = "/Users/James/Desktop/ml-latest-smallz/links.csv";
+	static private String movies = "C:\\Users\\James\\Desktop\\movies.csv";
+	static private String ratings = "/Users/James/Desktop/ml-latest-smallz/ratings.csv";
+	static private String tags = "/Users/James/Desktop/ml-latest-smallz/tags.csv";
+	static private BufferedReader br = null;
+	static 	private String line = "";
+    static private String cvsSplitBy = ",";
 	
 	
+	public Movie(){
+		this.movieID = -1;
+		this.imdbID = -1; 
+		this.tmdbID = -1;
+		this.title = "none";
+		this.rating = -0.1;
+	}
 
-	public Movie(int movieID, int imdbID, int tmdbID, String title, String genres, String timeStamp, String tag,
+	public Movie(int movieID, int imdbID, int tmdbID, String title,
 			double rating) {
 		
 		this.movieID = movieID;
-		this.imdbID = imdbID;
+		this.imdbID = imdbID; 
 		this.tmdbID = tmdbID;
 		this.title = title;
-		this.genres = genres;
-		this.timeStamp = timeStamp;
-		this.tag = tag;
 		this.rating = rating;
 	
 	}
 	
+	public String toString(){
+		return(this.getMovieID() + ", " + this.getTitle());
+	}
 	
+	public void setIdAndTitle(){
+		try (BufferedReader br = new BufferedReader(new FileReader(movies))) {
 
+            while ((line = br.readLine()) != null) {
+            	Random ran = new Random();
+            	
+                // use comma as separator
+                Object[] movie = line.split(cvsSplitBy);
+                if(movie[0].equals("movieId")){
+                }
+               
+            	
+            	else{
+            	int i = Integer.valueOf((String) movie[0]);
+                int q = ran.nextInt(16498);
+            	if (q == i){
+                this.setMovieID(i);
+                this.setTitle((String)(movie[1]));
+                
+                }
+            }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+	}
+
+	
 	public int getMovieID() {
 		return movieID;
 	}
